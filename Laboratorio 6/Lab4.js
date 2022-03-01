012ca8e384b01edd400d01385ba760482c18aa93
Andres <andresacecar@gmail.com>
2022-03-01 13:35:14 -0600

    Laboratorio 6 completado

--- Laboratorio 6/Lab4.js
+++ Laboratorio 6/Lab4.js
@@ -14,18 +14,25 @@ document.getElementById("precio_gorra").innerHTML = "$" + p_gorras + ".00";
 document.getElementById("precio_playera").innerHTML = "$" + p_playeras + ".00";
 document.getElementById("precio_pantalon").innerHTML = "$" + p_pantalon + ".00";
 
-let c_gorras = 0;
-let c_playeras = 0;
-let c_pantalones = 0;
+var c_gorras = 0;
+var c_playeras = 0;
+var c_pantalones = 0;
 
 document.getElementById("gorra_azul").onclick = () => {
     c_gorras++;
+    document.getElementById("car_gorra").innerHTML = "Gorras: " + c_gorras;
+    document.getElementById("total").innerHTML = "$" + ((c_gorras*p_gorras)+(c_playeras*p_playeras)+(c_pantalones*p_pantalon)) + ".00";
+    
 }
 
 document.getElementById("playera_negra").onclick = () => {
     c_playeras++;
+    document.getElementById("car_playera").innerHTML = "Playeras: " + c_playeras;
+    document.getElementById("total").innerHTML = "$" + ((c_gorras*p_gorras)+(c_playeras*p_playeras)+(c_pantalones*p_pantalon)) + ".00";
 }
 
 document.getElementById("pantalon_gris").onclick = () => {
     c_pantalones++;
+    document.getElementById("car_pantalon").innerHTML = "Pantalones: " + c_pantalones;
+    document.getElementById("total").innerHTML = "$" + ((c_gorras*p_gorras)+(c_playeras*p_playeras)+(c_pantalones*p_pantalon)) + ".00";
 }
